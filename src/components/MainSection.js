c445c563a242e5910d5a697bf4e214601aa9ac38
sidharthpunathil <sidharthpunathil714@gmail.com>
2021-09-05 01:55:30 +0530

    updated input logic

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -2,5 +2,13 @@ import Failed from "./Failed";
 import Data from "./Data";
 
 export default function MainSection({ userData }) {
-    return <>{userData ? <Data userData={userData} /> : <Failed />}</>;
+    return (
+        <>
+            {userData.message === undefined ? (
+                <Data userData={userData} />
+            ) : (
+                <Failed />
+            )}
+        </>
+    );
 }
