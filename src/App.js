be3abfa29dc8688889e30a8f63b8ba6702a42122
sidharthpunathil <sidharthpunathil714@gmail.com>
2021-09-05 01:04:41 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import NavBar from "./components/Navbar";
+import MainSection from "./components/MainSection";
+import { useState } from "react";
 
 function App() {
+    const [userData, setUserData] = useState(false);
     return (
         <>
-            <NavBar />
+            <NavBar setUserData={setUserData} />
+            <MainSection userData={userData} />
         </>
     );
 }
